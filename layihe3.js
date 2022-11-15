e8a7d0b58f1c6ea361ab3a72e8fb408d8e322898
turalagakisiyev <agakisiyevtural6@gmail.com>
2022-11-15 09:45:34 -0800

    ahh beee

--- layihe3.js
+++ layihe3.js
@@ -13,43 +13,23 @@ let input2 = document.getElementById('input2')
 let p2 = document.getElementById('p2')
 
 
+rub1.addEventListener('click',changeActive(rub1))
+usd1.addEventListener('click',changeActive(usd1))
+eur1.addEventListener('click',changeActive(eur1))
+gbp1.addEventListener('click',changeActive(gbp1))
+ 
 
-
-
-
-
-usd2.addEventListener('click', () => {
-    fetch('https://api.exchangerate.host/latest?base=USD&symbols=RUB')
-        .then(res => res.json())
-        .then((item) => {
-            p2.innerText = `1 USD = ${item.rates.RUB} RUB`
-            input1.value = input2.value * item.rates.RUB
+function changeActive(e){
+    if(e.className == "currency-item"){
+        const currencies = [...e.parentElement.children];
+        currencies.forEach(e => {
+            if(e.className == "active"){
+                e.classList.remove("active");
+            }
         })
-    usd2.style.backgroundColor = "#833AE0"
-    usd2.style.color = "white"
-})
-
-eur2.addEventListener('click', () => {
-    fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
-        .then(res => res.json())
-        .then((item) => {
-            p2.innerText = `1 RUB = ${item.rates.EUR} EUR`
-            input1.value = input2.value * item.rates.EUR
-        })
-    eur2.style.backgroundColor = "#833AE0"
-    eur2.style.color = "white"
-})
-
-rub1.addEventListener('click', () => {
-    fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD')
-        .then(res => res.json())
-        .then((item) => {
-            p1.innerText = `1 RUB = ${item.rates.USD} USD`
-            input2.value = input1.value * item.rates.USD
-        })
-    rub1.style.backgroundColor = "#833AE0"
-    rub1.style.color = "white"
-})
+        e.classList.add("active");
+    }
+}
 
 
 
@@ -57,69 +37,29 @@ rub1.addEventListener('click', () => {
 
 
 
-fetch('https://api.exchangerate.host/latest?base=USD&symbols=EUR')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.EUR)
-    })
-fetch('https://api.exchangerate.host/latest?base=USD&symbols=GBP')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.GBP)
-    })
 
 
 
 
 
-fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.EUR)
-    })
-fetch('https://api.exchangerate.host/latest?base=RUB&symbols=GBP')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.GBP)
-    })
 
 
 
 
-fetch('https://api.exchangerate.host/latest?base=EUR&symbols=USD')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.USD)
-    })
-fetch('https://api.exchangerate.host/latest?base=EUR&symbols=RUB')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.RUB)
-    })
-fetch('https://api.exchangerate.host/latest?base=EUR&symbols=GBP')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.GBP)
-    })
 
 
 
 
-fetch('https://api.exchangerate.host/latest?base=GBP&symbols=RUB')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.RUB)
-    })
-fetch('https://api.exchangerate.host/latest?base=GBP&symbols=USD')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.USD)
-    })
-fetch('https://api.exchangerate.host/latest?base=GBP&symbols=EUR')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.EUR)
-    })
+
+
+
+
+
+
+
+
+
+
 
 
 
