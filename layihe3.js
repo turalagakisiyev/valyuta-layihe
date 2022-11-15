e947cd9124d6cc19375c882749a36b37302f79c7
turalagakisiyev <agakisiyevtural6@gmail.com>
2022-11-15 15:45:52 +0400

    4commit

--- layihe3.js
+++ layihe3.js
@@ -15,11 +15,48 @@ let p2 = document.getElementById('p2')
 
 
 
-fetch('https://api.exchangerate.host/latest?base=USD&symbols=RUB')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.RUB)
-    })
+
+
+
+usd2.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=USD&symbols=RUB')
+        .then(res => res.json())
+        .then((item) => {
+            p2.innerText = `1 USD = ${item.rates.RUB} RUB`
+            input1.value = input2.value * item.rates.RUB
+        })
+    usd2.style.backgroundColor = "#833AE0"
+    usd2.style.color = "white"
+})
+
+eur2.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
+        .then(res => res.json())
+        .then((item) => {
+            p2.innerText = `1 RUB = ${item.rates.EUR} EUR`
+            input1.value = input2.value * item.rates.EUR
+        })
+    eur2.style.backgroundColor = "#833AE0"
+    eur2.style.color = "white"
+})
+
+rub1.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD')
+        .then(res => res.json())
+        .then((item) => {
+            p1.innerText = `1 RUB = ${item.rates.USD} USD`
+            input2.value = input1.value * item.rates.USD
+        })
+    rub1.style.backgroundColor = "#833AE0"
+    rub1.style.color = "white"
+})
+
+
+
+
+
+
+
 fetch('https://api.exchangerate.host/latest?base=USD&symbols=EUR')
     .then(res => res.json())
     .then((item) => {
@@ -34,11 +71,7 @@ fetch('https://api.exchangerate.host/latest?base=USD&symbols=GBP')
 
 
 
-fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD')
-    .then(res => res.json())
-    .then((item) => {
-        console.log(item.rates.USD)
-    })
+
 fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
     .then(res => res.json())
     .then((item) => {
