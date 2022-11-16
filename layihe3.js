99ed7a6ec9b71b7da6690882f1e10afa7c180aab
turalagakisiyev <agakisiyevtural6@gmail.com>
2022-11-16 15:35:16 +0400

    almost compele

--- layihe3.js
+++ layihe3.js
@@ -12,7 +12,6 @@ const gbp2 = document.getElementById('gbp2')
 let input2 = document.getElementById('input2')
 let p2 = document.getElementById('p2')
 
-
 rub1.addEventListener('click', changeActive)
 usd1.addEventListener('click', changeActive)
 eur1.addEventListener('click', changeActive)
@@ -24,12 +23,10 @@ eur2.addEventListener('click', changeActive)
 gbp2.addEventListener('click', changeActive)
 
 
-
 function changeActive(e) {
     if (e.target.className === "currency-item") {
         const currencies = [...e.target.parentElement.children];
         currencies.forEach(el => {
-
             if (el.classList.contains("active")) {
                 el.classList.remove("active")
             }
@@ -37,35 +34,40 @@ function changeActive(e) {
         e.target.classList.add("active");
     }
 
-
     // RUB
     if (rub1.classList.contains("active") && rub2.classList.contains("active")) {
-
         p1.innerText = `1 RUB = 1 RUB`
         input2.value = input1.value
         p2.innerText = `1 RUB = 1 RUB`
-
     }
 
     if (rub1.classList.contains("active") && usd2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 RUB = ${data.rates.USD} USD`
                 input2.value = input1.value * data.rates.USD
-
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=USD&symbols=RUB')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 USD = ${data.rates.RUB} RUB`
             })
-
-
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
 
 
     if (rub1.classList.contains("active") && eur2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
@@ -73,28 +75,43 @@ function changeActive(e) {
                 input2.value = input1.value * data.rates.EUR
 
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=RUB')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 EUR = ${data.rates.RUB} RUB`
             })
-
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
 
-
-
     if (rub1.classList.contains("active") && gbp2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 RUB = ${data.rates.GBP} GBP`
                 input2.value = input1.value * data.rates.GBP
-
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=RUB')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 GBP = ${data.rates.RUB} RUB`
+            })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
             })
     }
 
@@ -107,27 +124,48 @@ function changeActive(e) {
                 input2.value = input1.value * data.rates.RUB
 
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 RUB = ${data.rates.USD} USD`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
+
     if (usd1.classList.contains("active") && gbp2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=USD&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 USD = ${data.rates.GBP} GBP`
                 input2.value = input1.value * data.rates.GBP
-
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 GBP = ${data.rates.USD} USD`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
+
     if (usd1.classList.contains("active") && eur2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=USD&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
@@ -135,14 +173,23 @@ function changeActive(e) {
                 input2.value = input1.value * data.rates.EUR
 
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 EUR = ${data.rates.USD} USD`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
-    if (usd1.classList.contains("active") && usd2.classList.contains("active")) {
 
+    if (usd1.classList.contains("active") && usd2.classList.contains("active")) {
         p1.innerText = `1 USD = 1 USD`
         input2.value = input1.value
         p2.innerText = `1 USD = 1 USD`
@@ -151,97 +198,158 @@ function changeActive(e) {
 
     // EUR
     if (eur1.classList.contains("active") && eur2.classList.contains("active")) {
-
         p1.innerText = `1 EUR = 1 EUR`
         input2.value = input1.value
         p2.innerText = `1 EUR = 1 EUR`
-
     }
 
     if (eur1.classList.contains("active") && rub2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=RUB')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 EUR = ${data.rates.RUB} RUB`
                 input2.value = input1.value * data.rates.RUB
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 RUB = ${data.rates.EUR} EUR`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
+
     if (eur1.classList.contains("active") && usd2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 EUR = ${data.rates.USD} USD`
                 input2.value = input1.value * data.rates.USD
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=USD&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 USD = ${data.rates.EUR} EUR`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
+
     if (eur1.classList.contains("active") && gbp2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 EUR = ${data.rates.GBP} GBP`
                 input2.value = input1.value * data.rates.GBP
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 GBP = ${data.rates.EUR} EUR`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
 
     // GBP
     if (gbp1.classList.contains("active") && gbp2.classList.contains("active")) {
-
         p1.innerText = `1 GBP = 1 GBP`
         input2.value = input1.value
         p2.innerText = `1 GBP = 1 GBP`
-
     }
+
     if (gbp1.classList.contains("active") && rub2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=RUB')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 GBP = ${data.rates.RUB} RUB`
                 input2.value = input1.value * data.rates.RUB
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=RUB&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 RUB = ${data.rates.GBP} GBP`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+            
     } if (gbp1.classList.contains("active") && usd2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=USD')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 GBP = ${data.rates.USD} USD`
                 input2.value = input1.value * data.rates.USD
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=USD&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 USD = ${data.rates.GBP} GBP`
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
     }
+
     if (gbp1.classList.contains("active") && eur2.classList.contains("active")) {
+
         fetch('https://api.exchangerate.host/latest?base=GBP&symbols=EUR')
             .then(res => res.json())
             .then((data) => {
                 p1.innerText = `1 GBP = ${data.rates.EUR} EUR`
                 input2.value = input1.value * data.rates.EUR
             })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
+            })
+
         fetch('https://api.exchangerate.host/latest?base=EUR&symbols=GBP')
             .then(res => res.json())
             .then((data) => {
                 p2.innerText = `1 EUR = ${data.rates.GBP} GBP`
+            })
+            .catch(error => {
+                error = "Произошла ошибка"
+                alert(error);
             })
     }
 
